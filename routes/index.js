b77a14f3aacacb667a5719ae7243ad995231b943
sptrsca <sptrsca@gmail.com>
2023-11-01 16:01:48 -0400

    make index be better;

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,26 @@ router.get('/listener', (req, res) => {
     res.render('pages/listener')
 })
 
+router.get('/producer', (req, res) => {
+    const songs = [
+        { title: "Star", played: 100 },
+        { title: "Sunset", played: 200 },
+        { title: "Moonlight", played: 150 }
+    ];
+
+    const comments = [
+        "Comment 1",
+        "Comment 2",
+        "Comment 3",
+        "Comment 4"
+    ];
+
+    const trending = [
+        "Song A",
+        "Song B",
+        "Song C"
+    ];
+    res.render('pages/producer', { songs, comments, trending });
+})
+
 module.exports = router
